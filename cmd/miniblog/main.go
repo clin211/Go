454a3ae0fd66fc947efe80fe09e9dd3e0f6d3ff6
Forest <767425412lin@gmail.com>
2023-10-09 13:17:30 +0800

    feat: 实现命令行参数解析

--- cmd/miniblog/main.go
+++ cmd/miniblog/main.go
@@ -1,12 +1,22 @@
-// Copyright 2023 Forest-211(changlin) <767425412lin@gmail.com>. All rights reserved.
+// Copyright 2023 Forest-211(changlin) &lt;767425412lin@gmail.com>. All rights reserved.
 // Use of this source code is governed by a MIT style
 // license that can be found in the LICENSE file. The original repo for
 // this file is https://github.com/Forest-211/go/tree/miniblog.
 
 package main
 
-import "fmt"
+import (
+	"os"
+
+	// 使程序自动设置 GOMAXPROCS 以匹配 Linux 容器 CPU 配额。通过正确设置容器的 CPU 配额，可以解决 GOMAXPROCS 可能设置过大，导致生成线程过多，从而导致严重的上下文切换，浪费 CPU，降低程序性能的潜在问题
+	_ "go.uber.org/automaxprocs"
+
+	"github.com/Forest-211/miniblog/internal/miniblog"
+)
 
 func main() {
-	fmt.Println("Hello, miniblog!")
+	command := miniblog.NewMiniBlogCommand()
+	if err := command.Execute(); err != nil {
+		os.Exit(1)
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module github.com/Forest-211/miniblog
 
 go 1.19
+
+require github.com/spf13/cobra v1.7.0
+
+require (
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	go.uber.org/automaxprocs v1.5.3
+)
