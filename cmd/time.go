6c86346a9a00689cb9de5ed7cc10e2c65546addb
Forest <767425412lin@gmail.com>
2023-10-17 14:09:28 +0800

    feat: 添加时间戳解析

--- cmd/time.go
+++ cmd/time.go
@@ -64,9 +64,57 @@ var calculateTimeCmd = &cobra.Command{
 	},
 }
 
+var formatTimestamp = &cobra.Command{
+	Use:   "format",
+	Short: "时间戳格式化工具",
+	Long:  "将时间戳格式化为当前时区的标准时间",
+	Run: func(cmd *cobra.Command, args []string) {
+		timestamp := args[0]
+
+		// 解析时间戳的位数
+		timestampLen := len(timestamp)
+
+		var t time.Time
+		switch timestampLen {
+		case 10:
+			// 解析10位时间戳
+			seconds, err := strconv.ParseInt(timestamp, 10, 64)
+			if err != nil {
+				log.Fatal(err)
+			}
+			t = time.Unix(seconds, 0)
+		case 13:
+			// 解析13位时间戳
+			milliseconds, err := strconv.ParseInt(timestamp, 10, 64)
+			if err != nil {
+				log.Fatal(err)
+			}
+			seconds := milliseconds / 1000
+			t = time.Unix(seconds, milliseconds%1000*int64(time.Millisecond))
+		case 16:
+			// 解析16位时间戳
+			microseconds, err := strconv.ParseInt(timestamp, 10, 64)
+			if err != nil {
+				log.Fatal(err)
+			}
+			seconds := microseconds / 1000000
+			t = time.Unix(seconds, (microseconds%1000000)*int64(time.Microsecond))
+		default:
+			log.Fatal("无效的时间戳位数")
+		}
+
+		// 格式化为当前时区的标准时间
+		localTime := t.Local()
+
+		// 打印格式化后的时间
+		log.Println("格式化后的时间:", localTime.Format("2006-01-02 15:04:05"))
+	},
+}
+
 func init() {
 	timeCmd.AddCommand(nowTimeCmd)
 	timeCmd.AddCommand(calculateTimeCmd)
+	timeCmd.AddCommand(formatTimestamp)
 
 	calculateTimeCmd.Flags().StringVarP(&calculateTime, "calculate", "c", "", `需要计算的时间，有效单位为时间戳或已格式化的时间`)
 	calculateTimeCmd.Flags().StringVarP(&duration, "duration", "d", "", `持续时间，有效单位为: "ns", "us" (or "μs"), "ms", "s", "m", "h"`)
