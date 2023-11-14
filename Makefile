5609a341f13263ea47b4f701022bce763e9309d9
Forest <767425412lin@gmail.com>
2023-11-14 11:38:03 +0800

    fixed: 修复docker-compose up 不能启动的问题

--- Makefile
+++ Makefile
@@ -6,6 +6,8 @@ COMMON_SELF_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 ROOT_DIR := $(abspath $(shell cd $(COMMON_SELF_DIR)/ && pwd -P))
 # 构建产物、临时文件存放目录
 OUTPUT_DIR := $(ROOT_DIR)/_output
+# tmp 文件存放目录
+TMP_DIR := $(ROOT_DIR)/_tmp
 
 # ==============================================================================
 # 定义版本相关变量
@@ -67,3 +69,4 @@ clean: # 清理构建产物、临时文件等.
 .PHONY: clean
 clean: # 清理构建产物、临时文件等.
 	@-rm -vrf $(OUTPUT_DIR)
+	@-rm -vrf $(TMP_DIR)
--- internal/miniblog/helper.go
+++ internal/miniblog/helper.go
@@ -1,18 +1,15 @@
 package miniblog
 
 import (
-	"os"
-	"path/filepath"
 	"strings"
 
 	"github.com/Forest-211/miniblog/internal/pkg/log"
-	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
 const (
 	// recommendedHomeDir 定义放置 miniblog 服务配置的默认目录.
-	recommendedHomeDir = ".miniblog"
+	// recommendedHomeDir = ".miniblog"
 
 	// defaultConfigName 指定了 miniblog 服务的默认配置文件名.
 	defaultConfigName = "miniblog.yaml"
@@ -25,12 +22,14 @@ func initConfig() {
 		viper.SetConfigFile(cfgFile)
 	} else {
 		// 查找用户主目录
-		home, err := os.UserHomeDir()
+		// home, err := os.UserHomeDir()
 		// 如果获取用户主目录失败，打印 `'Error: xxx` 错误，并退出程序（退出码为 1）
-		cobra.CheckErr(err)
+		// cobra.CheckErr(err)
 
+		// fmt.Println("filepath.Join(home, recommendedHomeDir)", filepath.Join(home, recommendedHomeDir))
 		// 将用 `$HOME/<recommendedHomeDir>` 目录加入到配置文件的搜索路径中
-		viper.AddConfigPath(filepath.Join(home, recommendedHomeDir))
+		// viper.AddConfigPath(filepath.Join(home, recommendedHomeDir))
+		viper.AddConfigPath("configs")
 
 		// 把当前目录加入到配置文件的搜索路径中
 		viper.AddConfigPath(".")
