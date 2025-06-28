8a085129c43f14b701a41ccff168555fe4dbd931
forest-211 <767425412lin@gmail.com>
2025-06-28 10:09:59 +0800

    feat: 环境搭建并使用 air 热重载

---
+++ go-zero/user-center/user/internal/logic/userlogic.go
@@ -0,0 +1,30 @@
+package logic
+
+import (
+	"context"
+
+	"user/internal/svc"
+	"user/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type UserLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewUserLogic(ctx context.Context, svcCtx *svc.ServiceContext) *UserLogic {
+	return &UserLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *UserLogic) User(req *types.Request) (resp *types.Response, err error) {
+	return &types.Response{
+		Message: "Hello 我的第一个微服务！",
+	}, nil
+}
